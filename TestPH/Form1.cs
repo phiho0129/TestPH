b2e18368ec61f6e7c942086404de3c8fa1eca1db
huyhoang567 <hoangta567@gmail.com>
2018-10-09 15:23:31 +0700

    update

--- TestPH/Form1.cs
+++ TestPH/Form1.cs
@@ -16,9 +16,15 @@ namespace TestPH
             InitializeComponent();
             private void button_click(object sender, EventArgs e)
             {
+                if (textBox1.Text ! "") && (textbox2.text ! "")
                 double a = double.Parse(TxtA.Text);
                 double b = double.Parse(txtB.text);
             }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        
+        }
         }
     }
 }
